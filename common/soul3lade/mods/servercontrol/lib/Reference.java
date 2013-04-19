b56f09521bb9120463a14fa0c44fdb40bb8411c4
Soul3lade <Soul3lade16@gmail.com>
2013-04-18 23:50:55 -0400

    Added Main class and mcmod.info

---
+++ common/soul3lade/mods/servercontrol/lib/Reference.java
@@ -0,0 +1,13 @@
+package soul3lade.mods.servercontrol.lib;
+
+public class Reference {
+
+    
+    /*  Deceleration of Mod Constants    */
+    public static final String MOD_ID = "ServerControl";
+    public static final String MOD_NAME = "Server Control";
+    public static final String VERSION = "0.0.1";
+    
+    
+    
+}
